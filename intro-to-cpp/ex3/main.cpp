3b79b20f76afd72b464e0631fc06273f8c6cac39
Kanisk <57884127+chakrakan@users.noreply.github.com>
2020-10-14 02:44:25 -0400

    add: ex3

---
+++ intro-to-cpp/ex3/main.cpp
@@ -0,0 +1,59 @@
+#include <fstream>
+#include <iostream>
+#include <string>
+#include <vector>
+#include <sstream>
+using std::cout;
+using std::ifstream;
+using std::istringstream;
+using std::string;
+using std::vector;
+
+// TODO: Add the ParseLine function here.
+vector<int> ParseLine(string line)
+{
+    vector<int> nums;
+    char c;
+    int i;
+    istringstream myStream(line);
+    while (myStream >> i >> c)
+    {
+        nums.push_back(i);
+    }
+    return nums;
+}
+
+void ReadBoardFile(string path)
+{
+    ifstream myfile(path);
+    if (myfile)
+    {
+        string line;
+        while (getline(myfile, line))
+        {
+            cout << line << "\n";
+        }
+    }
+}
+
+void PrintBoard(const vector<vector<int>> board)
+{
+    for (int i = 0; i < board.size(); i++)
+    {
+        for (int j = 0; j < board[i].size(); j++)
+        {
+            cout << board[i][j];
+        }
+        cout << "\n";
+    }
+}
+
+#include "test.cpp" // For testing.
+
+int main()
+{
+    ReadBoardFile("1.board");
+    TestParseLine(); // For testing.
+                     // Leave commented out.
+                     // PrintBoard(board);
+}
